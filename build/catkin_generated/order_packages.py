b7bcd04cbd50ab5d8d06387157c4d6237d8465a9
xiaotong <h13021025522@163.com>
2020-03-27 17:46:42 +0800

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/hxt/catkin_my/talker_listener/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/hxt/Desktop/catkin_ws/devel;/home/hxt/catkin_my/cat_ws/devel;/home/hxt/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/hxt/Desktop/catkin_ws/devel;/home/hxt/catkin_my/cat_ws/devel;/home/hxt/catkin_ws/devel;/opt/ros/kinetic" != "" else []
